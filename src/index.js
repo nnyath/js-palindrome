51fe44545bed77730ac4baf062c62655cec8415a
Kenny Inthirath <kenny.ano.inthirath@hp.com>
2017-04-17 14:50:46 -0700

    - Added the JS API approach to the main program

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ var params = require('./config')
 var results = {
 }
 
-results.originalValidEven = {[params.even.validA] : Palindrome.original(params.even.validA,params.even.validIndex)}
-results.originalValidOdd = {[params.odd.validA] : Palindrome.original(params.odd.validA,params.odd.validIndex)}
+results.originalValidEven = { [params.even.validA]: Palindrome.original(params.even.validA, params.even.validIndex) }
+results.originalValidOdd = { [params.odd.validA]: Palindrome.original(params.odd.validA, params.odd.validIndex) }
 
-results.originalImprovedEven = {[params.even.validA] : Palindrome.originalImproved(params.even.validA)}
-results.originalImprovedOdd = {[params.odd.validA] : Palindrome.originalImproved(params.odd.validA)}
+results.originalImprovedEven = { [params.even.validA]: Palindrome.originalImproved(params.even.validA) }
+results.originalImprovedOdd = { [params.odd.validA]: Palindrome.originalImproved(params.odd.validA) }
 
+results.JSEven = { [params.even.validA]: Palindrome.JSAPI(params.even.validA) }
+results.JSOdd = { [params.odd.validA]: Palindrome.JSAPI(params.odd.validA) }
 
 console.log('Safe-Palindrome')
 console.log('---------------')
